6f63ee11ed08bc03c5c1940f2240c9b233317ad8
Alex <alexalexbabarika@gmail.com>
2026-04-20 22:57:05 +0100

    feat: add drawablesPersistence to centralize persistance logic of drawables

---
+++ frontend/src/lib/drawables/DrawablesPersistence.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { drawables } from './store.svelte';
+  import { loadAll, saveAll } from './persistence';
+
+  drawables.replaceAll(loadAll());
+
+  $effect(() => {
+    const items = drawables.items;
+    const id = setTimeout(() => {
+      saveAll(items);
+    }, 500);
+    return () => clearTimeout(id);
+  });
+</script>
